215cee6f64e8d03207802c6af5c89a15ee5eb22e
Opare Adams <opare.adams@gmail.com>
2019-06-03 11:08:22 +0000

    Update MiniStatistic.vue

--- src/components/widgets/statistic/MiniStatistic.vue
+++ src/components/widgets/statistic/MiniStatistic.vue
@@ -3,7 +3,12 @@
     <v-card-text class="pa-0">
       <v-container class="pa-0">
         <div class="layout row ma-0">
-          <div class="sm12 xs12 flex text-sm-center py-3 white--text" :class="color">
+         <div class="sm6 xs6 flex">
+            <div class="layout column ma-0 justify-center align-center">
+              <v-icon size="56px" :color="color">{{icon}}</v-icon>
+            </div>
+          </div>
+          <div class="sm6 xs6 flex text-sm-center py-3 white--text" :class="color">
             <div class="headline">{{ title }}</div>
             <span class="caption">{{ subTitle }}</span>
           </div>
