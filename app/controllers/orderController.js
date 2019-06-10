4d3ce08cd15c768256ffc0c801787076b4d5cd85
Opare Adams <opare.adams@gmail.com>
2019-06-10 14:28:37 +0000

    minor change

--- app/controllers/orderController.js
+++ app/controllers/orderController.js
@@ -213,7 +213,7 @@ exports.updateOrderStatus = (req, res) =>  {
 // Update Order packaged flag   
 exports.packagedOrderStatusUpdate = (req, res) =>  {
     Order.findOneAndUpdate(
-        {_id: req.params._id}
+        {orderId: req.params._id}
         ,{$set:{packaged:true,updatedAt:moment().format('YYYY-MM-DDTHH:mm:ss.SSS')}}
         ,{new:true}
     )
